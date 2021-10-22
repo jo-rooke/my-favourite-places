93e0d5c26a6363e63d456056e62d065b4549b17b
Jo <joely.rooke@gmail.com>
2021-10-22 15:54:23 +0100

    Built React App

--- src/components/FavPlaceItem.tsx
+++ src/components/FavPlaceItem.tsx
@@ -1,10 +1,32 @@
 interface FavPlaceItemProps {
-  username: string;
+  placeName: string;
+  imgSrc: string;
+  imgAlt: string;
+  location: string;
+  mapLink: string;
+  description: string;
+  proTip: string;
 }
 
 function FavPlaceItem(props: FavPlaceItemProps): JSX.Element {
   return (
-
+    <section>
+      <h2>
+        <strong>{props.placeName}</strong>
+      </h2>
+      <img src={props.imgSrc} />
+      <h3>
+        <strong>
+          {props.location} (<a href={props.mapLink}>map link</a>)
+        </strong>
+      </h3>
+      <p>{props.description}</p>
+      <p>
+        <em>
+          <strong>Pro tip:</strong> {props.proTip}
+        </em>
+      </p>
+    </section>
   );
 }
 
